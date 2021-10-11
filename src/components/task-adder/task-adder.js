abed3a75e7eeeb38ad40f04785250c628f715477
lizzochek <littled685@gmail.com>
2021-10-11 19:17:11 +0300

    Form for custom tasks addition

--- src/components/task-adder/task-adder.js
+++ src/components/task-adder/task-adder.js
@@ -3,16 +3,36 @@ import React from "react";
 import "./task-adder.css";
 
 export default class TaskAdder extends React.Component {
+  state = {
+    label: "",
+  };
+
+  onLabelChange = (event) => {
+    this.setState({
+      label: event.target.value,
+    });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.props.onAdd(this.state.label);
+
+    const input = document.querySelector("#input");
+    input.value = "";
+  };
+
   render() {
     return (
-      <div className="task-adder">
-        <button
-          className="btn btn-outline-secondary"
-          onClick={() => this.props.onAdd("Hello")}
-        >
-          Add Task
-        </button>
-      </div>
+      <form className="task-adder d-flex" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          id="input"
+          className="form-control"
+          onChange={this.onLabelChange}
+          placeholder="What needs to be done"
+        ></input>
+        <button className="btn btn-outline-secondary">Add Task</button>
+      </form>
     );
   }
 }
